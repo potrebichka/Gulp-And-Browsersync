7d27c3fefb67a6280a9864860c178d5ae620eb33
Nina Potrebich <potrebich@gmail.com>
2020-04-19 19:00:47 -0700

    Add bootstrap to project

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,9 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
-gulp.task('scripts', function() {
+
+
+gulp.task('scripts', function () {
   return gulp.src([
     /* Add your JS files here, they will be combined in this order */
     'src/js/main.js',
@@ -21,17 +23,24 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('./js'));
 });
 
-gulp.task('sass', function () {  
+
+
+gulp.task('sass', function() {  
     gulp.src('src/scss/main.scss')
         .pipe(sass({includePaths: ['scss']}))
         .pipe(gulp.dest('./css'))
         .pipe(minifycss());
 });
-gulp.task('html', function () {  
+
+
+
+gulp.task('html', function() {
     gulp.src('./*.html')
         .pipe(gulp.dest('./'));
 });
-gulp.task('browser-sync', function() {  
+
+
+gulp.task('browser-sync', function() {
     browserSync.init(["css/*.css", "js/*.js"], {
         server: {
             baseDir: "./"
@@ -39,7 +48,9 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('default', ['sass', 'browser-sync'], function () {  
+
+
+gulp.task('default', gulp.parallel('sass', 'browser-sync'), function () {
     gulp.watch("src/scss/**/*.scss", ['sass']).on("change", reload);
     gulp.watch("./*.html", ['html']).on("change", reload);
     gulp.watch("src/js/*.js", ['scripts']).on("change", reload);
